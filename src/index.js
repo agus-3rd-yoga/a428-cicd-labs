9aad7514b1c1753224251d5ed40d9071b16526be
agus-3rd-yoga <agus.3rd.yoga@gmail.com>
2023-12-20 12:03:37 -0500

    Add new app src

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
